147d14497948c877afb3e9521dc9406af70fca91
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2021-05-04 19:38:28 +0200

    Pump version and publish

--- lib/src/models/firestore_object.dart
+++ lib/src/models/firestore_object.dart
@@ -1,53 +1,27 @@
-import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:bdaya_firebase_helpers/src/models/json_based_object.dart';
 
 /// A base class for all firestore objects (for nested map objects, use [MapObject])
-class FirestoreDocumentBase implements JsonBasedObject {
-  Map<String, dynamic>? _data;
-  Map<String, dynamic> get data => _data ??= {};
-  set data(Map<String, dynamic> map) {
-    _data = map;
-  }
+abstract class FirestoreDocumentBase implements JsonBasedObject {
+  final String path;
+  final String id;
 
-  bool exists;
+  FirestoreDocumentBase({
+    required this.path,
+    required this.id,
+  });
 
-  final String path; // => snapshot.reference.path;
-  String get id => reference.id;
-
-  DocumentReference? _reference;
-  DocumentReference get reference =>
-      _reference ??= FirebaseFirestore.instance.doc(path);
-
-  Future<void> save() => reference.update(data);
-
-  Future<void> createOrUpdate() => reference.set(data, SetOptions(merge: true));
-
-  FirestoreDocumentBase(DocumentSnapshot<Map<String, dynamic>> __snapshot)
-      : _data = __snapshot.data(),
-        path = __snapshot.reference.path,
-        exists = __snapshot.exists;
-
-  FirestoreDocumentBase.fromMap(
-    this.path, {
-    bool exists = true,
+  void fromJson({
+    required String path,
     required Map<String, dynamic> data,
-  })  : exists = exists,
-        _data = data;
-
-  @override
-  String toString() =>
-      'FirestoreDocumentHelper(_data: $_data, _reference: $_reference)';
+  });
+  Map<String, dynamic> toJson();
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    return other is FirestoreDocumentBase &&
-        other.reference.path == other.reference.path;
+    return other is FirestoreDocumentBase && other.path == other.path;
   }
 
   @override
-  int get hashCode => reference.path.hashCode;
+  int get hashCode => path.hashCode;
 }
